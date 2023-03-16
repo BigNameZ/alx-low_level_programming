bab6c0d1b84981715a8c37e8026d4d0279fb1fcd
BigNameZ <akpovwovwooke@gmail.com>
2023-03-16 13:12:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+/**
+ * main - A puts function that prints a line
+ *
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (o);
+
+}
