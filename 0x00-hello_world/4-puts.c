a47ac2b5e7f55b3670f04c2d2a11d1760c14ea8a
BigNameZ <akpovwovwooke@gmail.com>
2023-03-16 14:08:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
- * main - A puts function that prints a line
- *
+ * main - A program that prints a line
  * Return:0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle,");
-return (0);
-
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
