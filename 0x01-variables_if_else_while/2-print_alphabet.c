927e4df55fbd7921e1f9f0a513820a5d76b96cd1
BigNameZ <akpovwovwooke@gmail.com>
2023-03-17 12:07:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lower case.
+ *
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
