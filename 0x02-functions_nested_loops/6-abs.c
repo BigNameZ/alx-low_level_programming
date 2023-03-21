b35a37078e5a96be2aa497f81a94a95f71f9fdb8
BigNameZ <akpovwovwooke@gmail.com>
2023-03-21 15:45:25 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,11 @@
 /**
  * _abs - computes the absolute value of an integer
  *
- * @n: parameter to be  checked
+ * @a: parameter to be checked
  *
  * Return: always a
  */
+
 int _abs(int a)
 {
 	if (a < 0)
