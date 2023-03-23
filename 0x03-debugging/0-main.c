451ebb64ec097540784984aa92eaa713370c3034
BigNameZ <akpovwovwooke@gmail.com>
2023-03-23 04:46:31 -0700

    1-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
-#include "main.h"
-
+#include"main.h"
 /**
  * main - Test function for positive or negative
  * Return: 0
@@ -9,8 +8,9 @@ int main(void)
 {
 	int i;
 
-	i = 0
+	i = 0;
 	positive_or_negative(i);
 
 return (0);
 }
+
