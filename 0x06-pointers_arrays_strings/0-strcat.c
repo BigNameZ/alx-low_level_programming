569221693f272ed46b02f7bd65dbc32559e25ac6
BigNameZ <akpovwovwooke@gmail.com>
2023-03-29 11:28:13 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,23 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int x;
-	int y;
+	int i;
+	int j;
 
 	i = 0;
-	while (dest[x] != '\0')
+	while (dest[i] != '\0')
 	{
-		x++;
+		i++;
 	}
 	j = 0;
-	while (src[y] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[x] = src[y];
-		x++;
-		y++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
 	dest[i] = '\0';
 	return (dest);
 }
-
